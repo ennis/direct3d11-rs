13f13e828da5173de782425416ba4acf4b910c37
Connie Hilarides <conni_h@outlook.com>
2018-04-26 22:30:48 -0700

    pub use main types, impl BackbufferTexture for Texture2D

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,16 @@ extern crate dxgi;
 extern crate winapi;
 extern crate wio;
 
+pub use device::Device;
+pub use device_context::DeviceContext;
+pub use error::Error;
+pub use texture2d::Texture2D;
+
 #[macro_use]
 pub mod helpers;
 
-pub mod device_context;
 pub mod device;
+pub mod device_context;
 pub mod error;
 pub mod flags;
 pub mod texture2d;
--- src/texture2d/mod.rs
+++ src/texture2d/mod.rs
@@ -1,8 +1,12 @@
 use device::Device;
 
 use dxgi::surface::Surface as DxgiSurface;
+use dxgi::swap_chain::BackbufferTexture;
+use winapi::ctypes::c_void;
 use winapi::shared::dxgi::IDXGISurface;
+use winapi::shared::guiddef::GUID;
 use winapi::um::d3d11::ID3D11Texture2D;
+use winapi::Interface;
 use wio::com::ComPtr;
 
 pub mod builder;
@@ -13,12 +17,31 @@ pub struct Texture2D {
 }
 
 impl Texture2D {
+    #[inline]
     pub fn create(device: &Device) -> builder::Texture2DBuilder {
         builder::Texture2DBuilder::new(device)
     }
 
+    #[inline]
     pub fn as_dxgi(&self) -> DxgiSurface {
         unsafe { DxgiSurface::from_raw(self.ptr.cast::<IDXGISurface>().unwrap().into_raw()) }
+    }
+
+    #[inline]
+    pub unsafe fn from_raw(raw: *mut ID3D11Texture2D) -> Self {
+        Self {
+            ptr: ComPtr::from_raw(raw),
+        }
+    }
+}
+
+unsafe impl BackbufferTexture for Texture2D {
+    fn uuidof() -> GUID {
+        ID3D11Texture2D::uuidof()
+    }
+
+    unsafe fn from_raw(raw: *mut c_void) -> Self {
+        Texture2D::from_raw(raw as _)
     }
 }
 
